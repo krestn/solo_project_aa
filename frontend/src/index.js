db5a0c8598c52cfb35665db075cf0f8a914eb0b0
Kreston <kreston.cm@gmail.com>
2022-01-09 20:44:42 -0600

    redux actions and reducer created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+
 
 
 
@@ -16,6 +18,8 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
+
 }
 
 function Root() {
