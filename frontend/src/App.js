5852a716367e4318081c85bfbea60db29fa8ddb3
Kreston <kreston.cm@gmail.com>
2022-01-13 12:52:29 -0600

    Updated Store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,38 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage";
-import * as sessionActions from "./store/session";
-import Navigation from "./components/Navigation";
+import React, { useState, useEffect } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import SignupFormPage from './components/Signup'
+import * as sessionActions from './store/session'
+import Navigation from './components/Navigation'
+import EventList from './components/EventList'
+import CreateEventForm from './components/CreateEventForm'
+import EditEventForm from './components/CreateEventForm'
 
 function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
+  const dispatch = useDispatch()
+  const [isLoaded, setIsLoaded] = useState(false)
+
   useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true))
+  }, [dispatch])
 
   return (
     <>
-      <Navigation isLoaded={isLoaded} />
+      <Navigation isLoaded={ isLoaded } />
       {isLoaded && (
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
+        <Route path='/signup'>
+          <SignupFormPage />
+        </Route>
+      </Switch>
       )}
+      <CreateEventForm />
+      <EventList />
+      <Route path='/events/:eventId'>
+        <EditEventForm />
+      </Route>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
